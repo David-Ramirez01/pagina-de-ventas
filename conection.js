907ed2623e0b62672c0b1faf7988a1ae556b4f7f
A_David <aimardavidninor@hotmail.com>
2022-11-24 10:47:41 -0500

    Place content API

--- conection.js
+++ conection.js
@@ -1,8 +1,22 @@
-
-
 let url = "https://fakestoreapi.com/products";
+const productos = document.getElementById('productos');
+const frag = document.createDocumentFragment();
+
 $.get(url , function(respuesta){
-    respuesta.forEach(function(item) {
-        console.log(item);
-    });
+    const data = respuesta;
+    console.log(data);
+        PrintCard(data);
 },"json");
+
+const PrintCard = data => {
+    const template = document.getElementById("tem-productos").content
+    data.forEach(item => {
+        template.querySelector('h5').textContent = item.title;
+        template.querySelector('p').textContent = item.price;
+        template.querySelector('img').setAttribute('src', item.image);
+        template.querySelector('button').dataset.id = item.id;
+        const clone = template.cloneNode(true);
+        frag.appendChild(clone);
+    })
+    productos.appendChild(frag);
+}
